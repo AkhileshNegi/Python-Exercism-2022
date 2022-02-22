351cb33c24cf809b7afda1f8518845386074d87a
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-02-22 23:53:48 +0530

    added doc string

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,8 @@
 def two_fer(name='you'):
-   return 'One for ' + name + ', one for me.'
+    '''
+    :param name: str of a root name
+    :return:  returns string with either name entered or default value `you` being replaced in string
+    This function takes `name` as a parameter and
+    returns string.
+    '''
+    return 'One for {name}, one for me.'.format(name=name)
